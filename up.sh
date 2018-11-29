50fa25246ee1cd0e6f13d4dcdfc7bb806f50b12d
Yadhu Prakash <yadhu.prakash@procore.com>
2018-11-28 16:50:45 -0800

    Add homebrew, cask and wrap.

---
+++ up.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+if [ -x "$(command -v brew)" ]; then
+  echo "Brew already installed"
+else
+  /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+fi
+
+#Install oh-my-szh
+
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+./install.sh
+
+./macos.sh
+
+brew install rcmdnk/file/brew-file
+brew bundle
+
+# Install vim-plug Plugin Manager
+ curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
